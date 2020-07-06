3b106167b65f2ec396c3669015154b8126946fa5
unknown <lij15632@noahgroup.com.local>
2020-07-06 15:09:40 +0800

    set

--- test/data_type.py
+++ test/data_type.py
@@ -109,8 +109,37 @@ print('tup[1:5]=', tup[1:5
 tup1 = ()    # 空元组
 tup2 = (20,) # 一个元素，需要在元素后添加逗号
 
+"""
+set:集合（set）是由一个或数个形态各异的大小整体组成的，构成集合的事物或对象称作元素或是成员。
+
+基本功能是进行成员关系测试和删除重复元素。
+
+可以使用大括号 { } 或者 set() 函数创建集合，注意：创建一个空集合必须用 set() 而不是 { }，因为 { } 是用来创建一个空字典。
+"""
+sites = {'Google', 'Taobao', 'Runoob', 'Facebook', 'Zhihu', 'Baidu'}
+
+print(sites)   # 输出集合，重复的元素被自动去掉
+
+# 成员测试
+if 'Runoob' in sites :
+    print('Runoob 在集合中')
+else :
+    print('Runoob 不在集合中')
 
 
+# set可以进行集合运算
+a = set('abracadabra')
+b = set('alacazam')
+
+print('集合a=', a)
+
+print('a和b的差集=', a - b)     # a 和 b 的差集
+
+print('a和b的并集=', a | b)     # a 和 b 的并集
+
+print('a和b的交集=', a & b)     # a 和 b 的交集
+
+print('a和b中不同时存在的元素='a ^ b)     # a 和 b 中不同时存在的元素
 
 
 
