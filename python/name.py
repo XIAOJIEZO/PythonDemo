b4f2e16b118160e4b5b5749786d7141cfb47578b
unknown <lij15632@noahgroup.com.local>
2020-07-08 18:11:47 +0800

    string

--- python/name.py
+++ python/name.py
@@ -1,25 +1,3 @@
-print("hello python world")
-
-# 第一个注释
-print('hello ,python!')
-
-# \ 转义符  r去掉转义
-print('转义,\n换行')
-print(r'转义,\n换行')
-
-# 等待用户输入
-a = input('请输入，按回车结束：')
-print('你输入的内容是：'+a)
-
-# print输出默认换行，变量末尾加 end=""不换行
-x = "a"
-y = "b"
-print(x)
-print(y)
-
-print(x, end="")
-print(y)
-
 """
 变量的命名和使用:
 1、只能包含字母、数字和下划线
