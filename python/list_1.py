7f1da19350487d5d8b0dd8fd54a5636dd58be238
unknown <lij15632@noahgroup.com.local>
2020-07-10 11:04:23 +0800

    练习

--- python/list_1.py
+++ python/list_1.py
@@ -36,3 +36,59 @@ print('和为：', sum(digits))
 # 列表解析，前面生成列表使用了三四行代码，列表解析只用一行代码就能生成这样的列表
 digits_1 = [i**2 for i in range(0, 10)] # 首先指定一个列表名，指定一个左方括号，定义一个表达式（i**2）,用于生成储存到列表中的值，编写一个for循环，给表达式提供值，再加上右方括号
 print(digits_1)
+
+"""
+练习：
+1、使用for循环，打印数字1~20
+2、创建一个列表，包含数字1~1000000，再使用for循环将这些数字打印出来
+3、计算列表的最大值、最小值以及和
+4、创建列表，包含1~20的奇数，再使用for循环将这些数字打印出来
+5、创建一个列表，包含3~30能被3整除的数字，再使用for循环将这些数字打印出来
+6、创建一个列表，其中包含1~10的立方，再使用for循环将这些数字打印出来
+7、使用列表解析生成一个列表，其中包含1~10的立方
+"""
+# 1
+for i in range(1, 21):
+    print(i)
+
+# 2
+list_1 = []
+#for i in range(1, 1000001):
+#    list_1.append(i)        
+list_1 = list(range(1, 1000001))    # 优化写法
+
+for i in list_1:
+    print(i)
+    
+# 3
+print('min(list_1)=', min(list_1))
+print('max(list_1)=', max(list_1))
+print('sum(list_1)=', sum(list_1))
+
+# 4
+odd_numbers = []
+for odd_number in range(1, 21, 2):
+    odd_numbers.append(odd_number)
+
+for odd_number in odd_numbers:
+    print(odd_number)
+    
+# 5
+multiple_3 = []
+for num in range(3, 31, 3):
+    multiple_3.append(num)
+
+for num in multiple_3:
+    print(num)
+    
+# 6
+cube = []
+for num in range(1, 11):
+    cube.append(num**3)
+
+for num in cube:
+    print(num)
+    
+# 7
+cube_1 = [num**3 for num in range(1, 11)]
+print(cube_1)
